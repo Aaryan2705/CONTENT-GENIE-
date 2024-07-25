d212582b7162587ebd555897b2e4379314dff70c
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-07-26 02:45:50 +0530

    Delete drizzle.config.js

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: "./utils/schema.tsx",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://AI-Content-Generator_owner:GB3bZcpdf6kU@ep-muddy-bonus-a5szmhsh.us-east-2.aws.neon.tech/AI-Content-Generator?sslmode=require',
-    }
-  };
